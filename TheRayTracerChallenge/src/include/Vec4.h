599fd9de9a8900a899a586a81309c3d342fc7ebd
Markus Glau <glaumarkus1209@gmail.com>
2020-11-20 11:50:27 +0100

    Small move optimization

--- TheRayTracerChallenge/src/include/Vec4.h
+++ TheRayTracerChallenge/src/include/Vec4.h
@@ -130,73 +130,82 @@ struct Vec4 {
 		else return 0.0f;
 	}
 
+	float operator [](const int& idx) const {
+		if (idx == 0) return x;
+		else if (idx == 1) return y;
+		else if (idx == 2) return z;
+		else if (idx == 3) return w;
+		else return 0.0f;
+	}
+
 };
 
 
 // add operators
-inline Vec4 operator +(const Vec4& v1, const Vec4& v2)
+inline Vec4&& operator +(const Vec4& v1, const Vec4& v2)
 {
-	return Vec4(v1.x + v2.x,
+	return std::move(Vec4(v1.x + v2.x,
 		v1.y + v2.y,
 		v1.z + v2.z,
-		v1.w + v2.w);
+		v1.w + v2.w));
 }
 
-inline Vec4 operator -(const Vec4& v1, const Vec4& v2)
+inline Vec4&& operator -(const Vec4& v1, const Vec4& v2)
 {
-	return Vec4(v1.x - v2.x,
+	return std::move(Vec4(v1.x - v2.x,
 		v1.y - v2.y,
 		v1.z - v2.z,
-		v1.w - v2.w);
+		v1.w - v2.w));
 }
 
-inline Vec4 operator *(const Vec4& v1, const Vec4& v2)
+inline Vec4&& operator *(const Vec4& v1, const Vec4& v2)
 {
-	return Vec4(v1.x * v2.x,
+	return std::move(Vec4(v1.x * v2.x,
 		v1.y * v2.y,
 		v1.z * v2.z,
-		v1.w * v2.w);
+		v1.w * v2.w));
 }
 
-inline Vec4 operator *(const Vec4& v, float f)
+inline Vec4&& operator *(const Vec4& v, float f)
 {
-	return Vec4(v.x * f,
+	return std::move(Vec4(v.x * f,
 		v.y * f,
 		v.z * f,
-		v.w * f);
+		v.w * f));
 }
 
-inline Vec4 operator *(float f, const Vec4& v)
+inline Vec4&& operator *(float f, const Vec4& v)
 {
-	return Vec4(f * v.x,
+	return std::move(Vec4(f * v.x,
 		f * v.y,
 		f * v.z,
-		f * v.w);
+		f * v.w));
 }
 
-inline Vec4 operator /(const Vec4& v1, const Vec4& v2)
+inline Vec4&& operator /(const Vec4& v1, const Vec4& v2)
 {
-	return Vec4(v1.x / v2.x,
+	return std::move(Vec4(v1.x / v2.x,
 		v1.y / v2.y,
 		v1.z / v2.z,
-		v1.w / v2.w);
+		v1.w / v2.w));
 }
 
-inline Vec4 operator /(const Vec4& v, float f)
+inline Vec4&& operator /(const Vec4& v, float f)
 {
-	return Vec4(v.x / f,
+	return std::move(Vec4(v.x / f,
 		v.y / f,
 		v.z / f,
-		v.w / f);
+		v.w / f));
 }
 
-inline Vec4 operator /(float f, const Vec4& v)
+inline Vec4&& operator /(float f, const Vec4& v)
 {
-	return Vec4(f / v.x,
+	return std::move(Vec4(f / v.x,
 		f / v.y,
 		f / v.z,
-		f / v.w);
+		f / v.w));
 }
+
 
 // Vector operations
 
@@ -206,16 +215,16 @@ float dot(const Vec4& v1, const Vec4& v2) {
 }
 
 // cross
-Vec4 cross(const Vec4& v1, const Vec4& v2) {
+Vec4&& cross(const Vec4& v1, const Vec4& v2) {
 	return
-		Vec4(v1.y * v2.z - v1.z * v2.y,
+		std::move(Vec4(v1.y * v2.z - v1.z * v2.y,
 			v1.z * v2.x - v1.x * v2.z,
-			v1.x * v2.y - v1.y * v2.x);
+			v1.x * v2.y - v1.y * v2.x));
 }
 
 // reflect
-Vec4 reflect(const Vec4& v1, const Vec4& v2) {
-	return Vec4(v1 - v2 * 2.0f * dot(v1, v2));
+Vec4&& reflect(const Vec4& v1, const Vec4& v2) {
+	return std::move(Vec4(v1 - v2 * 2.0f * dot(v1, v2)));
 }
 
 // basic operations
@@ -245,10 +254,6 @@ struct Point : public Vec4 {
 
 };
 
-void unit_test_vec4() {
-
-	// insert unit tests here
-}
 
 }
 
